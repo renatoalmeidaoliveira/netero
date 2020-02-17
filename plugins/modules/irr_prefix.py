41c6d9c3e955925af2063b22993c05c88d89f896
Renato Almeida de Oliveira <renato.almeida.oliveira@pm.me>
2020-02-16 21:16:48 -0300

    Create irr_prefix module

---
+++ plugins/modules/irr_prefix.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+from ansible.errors import AnsibleError
+from ansible.module_utils.basic import to_native, AnsibleModule
+import datetime
+ANSIBLE_METADATA = {
+    'metadata_version': '1.0.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: irr_prefix
+
+short_description: Generater IRR prefix-list
+
+version_added: "0.0.1"
+
+description:
+    - "This modules runs bgpq3 to generate model based prefix-list"
+
+options:
+    asn32Safe:
+        description:
+            - "assume that your device is asn32-safe"
+        required: false
+        default: false
+    IPv:
+        description:
+            - "IP protocol version"
+        required: true
+        choices: [ 4 , 6]
+    aggregate:
+        description:
+            - "If true aggregate the prefix"
+        required: false
+        default: false
+    asSet:
+        descriptio:
+            - "The ASN IRR AS-SET"
+        required: true
+requirements:
+    - bgpq3
+author:
+    - Renato Almeida de Oliveira (renato.a.oliveira@pm.me)
+'''
+
+EXAMPLES = '''
+# Get as prefix-list
+- name: Get prefix-list
+  irr_prefix:
+    asn32_safe: true
+    IPv: 4
+    as-set: AS1234
+
+'''
+
+
+def main():
+
+    fields = {
+        "asn32Safe":    {"default": False, "type": "bool"},
+        "IPv":          {"required": True, "type": "str", choices=['4', '6']},
+        "aggregate":    {"default": False, "type": "bool"},
+        "asSet":        {"required": True, "type": "str"}
+
+    }
+    module = AnsibleModule(argument_spec=fields)
+    path = module.get_bin_path('bgpq3', True)
+    result = dict(changed=False, warnings=list())
+    try:
+        result.update(changed=True, message=to_native(path))
+    except Exception as e:
+        module.fail_json(msg=to_native(e))
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
