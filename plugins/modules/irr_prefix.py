e986bc474415799b2766c55d58f92aba5f59f395
Renato Almeida de Oliveira <renato.almeida.oliveira@pm.me>
2020-02-16 21:38:12 -0300

    Add bgpQuery fn

--- plugins/modules/irr_prefix.py
+++ plugins/modules/irr_prefix.py
@@ -57,11 +57,24 @@ EXAMPLES = '''
 '''
 
 
+def bgpq3Query(module, path):
+    args = module.params["IPv"]
+    if module.params["asn32Safe"]:
+        args = args + "3"
+    if module.params["aggregate"]:
+        args = args + "A"
+    cmd = "%s -%s -l irr_prefix %s", (path, args, module.params["asSet"])
+    rc, stdout, stderr = module.run_command(cmd, cwd=repo_path)
+    if stderr != "":
+        raise AnsibleError(" bgpq3 error: %s " % to_native(stderr))
+    return stdout
+
+
 def main():
 
     fields = {
         "asn32Safe":    {"default": False, "type": "bool"},
-        "IPv":          {"required": True, "type": "str", choices=['4', '6']},
+        "IPv":          {"required": True, "type": "str", "choices": ['4', '6']},
         "aggregate":    {"default": False, "type": "bool"},
         "asSet":        {"required": True, "type": "str"}
 
@@ -70,7 +83,8 @@ def main():
     path = module.get_bin_path('bgpq3', True)
     result = dict(changed=False, warnings=list())
     try:
-        result.update(changed=True, message=to_native(path))
+        response = bgpq3Query(module, path)
+        result.update(changed=True, message=to_native(response))
     except Exception as e:
         module.fail_json(msg=to_native(e))
     module.exit_json(**result)
